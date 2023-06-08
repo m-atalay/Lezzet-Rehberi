b4b14ea7ff95589482b8403148232880ed194dc4
m-atalay <133883869+m-atalay@users.noreply.github.com>
2023-06-08 20:06:01 +0300

    Add files via upload

---
+++ Arama sayfası.java
@@ -0,0 +1,29 @@
+public List<Recipe> searchRecipes(String keyword) {
+    List<Recipe> recipeList = new ArrayList<>();
+
+    SQLiteDatabase db = this.getReadableDatabase();
+
+    String selectQuery = "SELECT * FROM " + TABLE_RECIPES + " WHERE " + COLUMN_RECIPE_NAME + " LIKE '%" + keyword + "%'";
+
+    Cursor cursor = db.rawQuery(selectQuery, null);
+
+    if (cursor.moveToFirst()) {
+        do {
+            Recipe recipe = new Recipe();
+            recipe.setId(cursor.getInt(cursor.getColumnIndex(COLUMN_RECIPE_ID)));
+            recipe.setName(cursor.getString(cursor.getColumnIndex(COLUMN_RECIPE_NAME)));
+            recipe.setCategory(cursor.getString(cursor.getColumnIndex(COLUMN_RECIPE_CATEGORY)));
+            recipe.setIngredients(cursor.getString(cursor.getColumnIndex(COLUMN_RECIPE_INGREDIENTS)));
+            recipe.setInstructions(cursor.getString(cursor.getColumnIndex(COLUMN_RECIPE_INSTRUCTIONS)));
+            recipe.setServings(cursor.getInt(cursor.getColumnIndex(COLUMN_RECIPE_SERVINGS)));
+            recipe.setCookTime(cursor.getInt(cursor.getColumnIndex(COLUMN_RECIPE_COOK_TIME)));
+            recipe.setImage(cursor.getString(cursor.getColumnIndex(COLUMN_RECIPE_IMAGE)));
+            recipeList.add(recipe);
+        } while (cursor.moveToNext());
+    }
+
+    cursor.close();
+    db.close();
+
+    return recipeList;
+}
